0664e23cd364903a9f3da9263a804c50e5841d68
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-07 16:34:35 +0900

    📝Node 기초

--- NODE-API/index.js
+++ NODE-API/index.js
@@ -1,9 +1,23 @@
-// const math = require('./math.js'); // 생성한 모듈을 사용
-// const result = math.sum(1, 2);
-// console.log(result);
-const fs = require('fs');
-// const data = fs.readFileSync('./data.txt', 'utf-8');
-// console.log(data)
-const data = fs.readFile('./data.txt', 'utf-8', function (err, data) {
-  console.log(data);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  } else if (req.url === '/users') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('User list');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
