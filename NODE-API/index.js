fc0f3781b7f73f2dbb5a635f5e56e3790f3bc3f4
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-10 11:16:28 +0900

    📝REST API(인프런 김정환님)

--- NODE-API/index.js
+++ NODE-API/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+let users = [
+  { id: 1, name: 'jong' },
+  { id: 2, name: 'hyuk' },
+  { id: 3, name: 'woo' },
+];
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(morgan('dev'));
+
+app.get('/users', function (req, res) {
+  res.json(users);
 });
 
 app.listen(3000, function () {
