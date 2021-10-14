880c8e840d4e3f45c49aa0bb214e52766a8a29a7
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-10-14 09:59:28 +0900

    📝ORM 동기화(인프런 김정환님)

--- NODE-API/bin/www.js
+++ NODE-API/bin/www.js
@@ -1,5 +1,9 @@
 const app = require('../index');
+const syncDb = require('./sync-db');
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+syncDb().then((_) => {
+  console.log('데이터베이스 연결!');
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
 });
---
+++ NODE-API/models.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+});
+
+const User = sequelize.define('User', {
+  name: Sequelize.DataTypes.STRING,
+});
+
+module.exports = { Sequelize, sequelize, User };
